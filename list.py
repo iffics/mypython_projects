e45cc53bbc27105ca945fc5488c24d2a6ccec00e
Mohammad <iffi.cs777@gmail.com>
2017-11-14 06:26:36 -0800

    Some build-in Fucntion added with example

--- list.py
+++ list.py
@@ -20,6 +20,22 @@ while True:
 
     counter += 1
 
-    time.sleep(2)
+    # time.sleep(2)
     if a == 6:
         break
+print("----------------")
+
+# SOME USEFUL LIST FUNCTIONS
+
+testing = ['irfan', 'khan']
+
+# testing.clear() # this will clear all the list
+# testing.append() # this will add to end of the list
+# testing.copy()# this will copy the entire list
+# testing.count() # this is return how many times a item is in the list
+# testing.extend()# this will add new list to the existing list
+# testing.index('irfan')  # given an item of list will tell the index of it
+# testing.pop() # will remove th last item of the list
+# testing.reverse() # this is reverse the list items
+# testing.sort() # this is sort the list
+# testing.remove() # this is will remove the the specific item from the list
